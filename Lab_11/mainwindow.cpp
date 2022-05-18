e65c025bd45fa753523883d192e6a8a8aa6d875c
IlorDash <ilordash@mail.ru>
2022-05-18 21:57:52 +0300

    verticalLayout replaced by widget for MyWidget

--- Lab_11/mainwindow.cpp
+++ Lab_11/mainwindow.cpp
@@ -7,21 +7,8 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     // Создаем виджет
-    wgt = new MyWidget(this);
+    wgt = new MyWidget(ui->centralwidget);
 
-    // И кладём его в лайаут
-    ui->verticalLayout->addWidget(wgt);
-}
-
-void MainWindow::deleteFig() {
-    qDebug() << "In delete fig";
-    for (int it = 0; it < figArray.size(); it++) {
-        if (figArray[it].get() == hoveredFig) {
-            figArray.erase(figArray.begin()+it);
-            update();
-            return;
-        }
-    }
 }
 
 MainWindow::~MainWindow()
