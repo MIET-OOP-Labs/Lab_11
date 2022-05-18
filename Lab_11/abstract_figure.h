e9fb874128dbf9b16692862711f86f1cc4f5b64b
IlorDash <ilordash@mail.ru>
2022-05-18 21:58:24 +0300

    updated class with templates

--- Lab_11/abstract_figure.h
+++ Lab_11/abstract_figure.h
@@ -2,43 +2,31 @@
 #define ABSTRACTFIGURE_H
 
 #include <QPainter>
-#include <list>
-#include <QPoint>
 
 using namespace std;
 
+template<typename TModel>
 class AbstractFigure {
-	protected:
-        list<int> params = {};
-
-        int paramsNum = 0;
-
+    private:
         const string figName = "Abstract";
-
-
 		QColor myColor = Qt::red;
 		int myWidth = 1;
 		Qt::PenStyle myPenStyle = Qt::SolidLine;
-		int listGetItem(list<int> _list, int index);
+    public:
+        AbstractFigure();
 
-	public:
-		AbstractFigure();
-        virtual void drawFigure(QPainter *painter) = 0;
-
-		void setColor(QColor newColor) {myColor = newColor;};
-		void setWidth(int newWidth) {myWidth = newWidth;};
-		void setStyle(Qt::PenStyle newPenStyle) {myPenStyle = newPenStyle;};
+        void setColor(const QColor newColor) {myColor = newColor;};
+        void setWidth(const int newWidth) {myWidth = newWidth;};
+        void setStyle(const Qt::PenStyle newPenStyle) {myPenStyle = newPenStyle;};
 
 		QColor getColor() {return myColor;};
 		int getWidth() {return myWidth;};
 		Qt::PenStyle getStyle() {return myPenStyle;};
 
-		void setParams(list<int> newParams);
 
-        virtual void moveFig(int to_x, int to_y) = 0;
-
-        virtual bool isMouseInside(QPoint mousePos){return false;};
-
+        virtual void drawFigure() = 0;
+        virtual void moveFig(TModel dest) = 0;
+        virtual bool isMouseInside(TModel mousePos) = 0;
         virtual string getName() {return figName;};
 
         virtual ~AbstractFigure();
